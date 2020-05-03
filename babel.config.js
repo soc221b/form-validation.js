397006a03a8745473a9bb5db5b4221f6630580d0
Ernest <iattempt.net@gmail.com>
2020-05-03 13:50:21 +0800

    feat: transform all 2015+

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
+    '@babel/preset-env',
     '@babel/preset-typescript',
   ],
 };
