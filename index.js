5b0934da4bc191b83764cd77365fc79cda2ff40a
Ernest <iattempt.net@gmail.com>
2020-05-06 21:20:33 +0800

    feat: specify builds

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const pkg = require('./package.json')
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require(`./dist/${pkg.name}.cjs.prod.js`)
-} else {
-  module.exports = require(`./dist/${pkg.name}.cjs.js`)
-}
