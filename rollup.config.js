d56e5839cab51e07ca68eeda1cbf259838d7ace6
Ernest <iattempt.net@gmail.com>
2020-05-06 20:39:18 +0800

    refactor: simplify code

--- rollup.config.js
+++ rollup.config.js
@@ -1,73 +1,44 @@
-export default commandLineArgs => {
-  const rm = require('rimraf')
-  const path = require('path')
-  const ts = require('rollup-plugin-typescript2')
-  const terser = require('rollup-plugin-terser').terser
-  const pkg = require(path.resolve(`package.json`))
+import rm from 'rimraf'
+import path from 'path'
+import ts from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
 
-  rm.sync(path.resolve('dist/**/*'))
+rm.sync(path.resolve('dist/**/*'))
 
-  const configs = {
-    esm: {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.esm.js`),
-        format: `es`,
-      },
-      plugins: [ts()],
-    },
-    cjs: {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.cjs.js`),
-        format: `cjs`,
-      },
-      plugins: [ts()],
-    },
-    global: {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.global.js`),
-        format: `iife`,
-        name: pkg.name
-          .split(/[^\w]/)
-          .map(n => n.slice(0, 1).toUpperCase() + n.slice(1))
-          .join(''),
-        extend: true,
-      },
-      plugins: [ts()],
-    },
+const packageName = 'form-validation'
+const pascalCasePackageName = 'Form-Validation'
 
-    'esm-prod': {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.esm.prod.js`),
-        format: `es`,
-      },
-      plugins: [ts(), terser()],
-    },
-    'cjs-prod': {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.cjs.prod.js`),
-        format: `cjs`,
-      },
-      plugins: [ts(), terser()],
-    },
-    'global-prod': {
-      input: 'src/index.ts',
-      output: {
-        file: path.resolve(`dist/${pkg.name}.global.prod.js`),
-        format: `iife`,
-        name: pkg.name
-          .split(/[^\w]/)
-          .map(n => n.slice(0, 1).toUpperCase() + n.slice(1))
-          .join(''),
-        extend: true,
-      },
-      plugins: [ts(), terser()],
+const input = 'src/index.ts'
+const formats = ['es', 'umd', 'amd', 'cjs', 'iife', 'system']
+const plugins = [ts()]
+
+const configs = []
+formats.forEach(format => {
+  const config = {
+    input,
+    plugins,
+    output: {
+      format,
+      name: pascalCasePackageName,
+      extend: true,
     },
   }
 
-  return Object.values(configs)
-}
+  configs.push({
+    ...config,
+    output: {
+      ...config.output,
+      file: path.resolve(`dist/${packageName}.${format}.js`),
+    },
+  })
+
+  configs.push({
+    ...config,
+    plugins: [...config.plugins, terser()],
+    output: {
+      ...config.output,
+      file: path.resolve(`dist/${packageName}.${format}.prod.js`),
+    },
+  })
+})
+export default configs
