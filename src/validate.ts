f3ff8514651126ac7f4a5057d53bbf12d65967f8
Ernest <iattempt.net@gmail.com>
2020-06-25 17:52:20 +0800

    refactor

--- src/validate.ts
+++ src/validate.ts
@@ -1,19 +1,5 @@
 import { getByPath } from './util'
 
-export interface ISchema {
-  $params: { [key: string]: any }
-  $rules: { [key: string]: ({}: IFunctionParams) => any }
-  $errors: { [key: string]: ({}: IFunctionParams) => any }
-  // TODO: use recursive definition to highlight nesting types
-  [key: string]: any
-}
-export interface IInstance {
-  _rootModel: any
-  _path: string
-  $errors: { [key: string]: any }
-  // TODO: use recursive definition to highlight nesting types
-  [key: string]: any
-}
 export interface IFunctionParams {
   value: any
   key?: string
@@ -23,18 +9,32 @@ export interface IFunctionParams {
   params: { [key: string]: any }
 }
 
-export const validate = ({ schema, instance }: { schema: ISchema; instance: IInstance }) => {
-  const path = instance._path === '' ? undefined : instance._path.split('.')
-  const value = path === undefined ? instance._rootModel : getByPath(instance._rootModel, path)
-  const key = path === undefined ? undefined : path[path.length - 1]
-  const parent = path === undefined || path.length === 1 ? undefined : getByPath(instance._rootModel, path.slice(0, -1))
-  const root = instance._rootModel
-  const params = schema.$params
+export interface IValidateParams {
+  rules: { [key: string]: ({}: IFunctionParams) => any }
+  errors: { [key: string]: ({}: IFunctionParams) => any }
+  root: any
+  path: string[]
+  params: { [key: string]: any }
+}
 
-  for (const ruleKey of Object.keys(schema.$rules)) {
-    const functionParams = { value, key, parent, path, root, params }
-    if (schema.$rules[ruleKey](functionParams) !== undefined) {
-      instance.$errors[ruleKey] = schema.$errors[ruleKey](functionParams)
+export interface IValidateResult {
+  $rulesResult: { [key: string]: any }
+  [key: string]: any
+}
+
+export const validate = ({ rules, errors, root, path, params }: IValidateParams): IValidateResult => {
+  const value = path === undefined ? root : getByPath(root, path)
+  const key = path === undefined ? undefined : path[path.length - 1]
+  const parent = path.length <= 1 ? undefined : getByPath(root, path.slice(0, -1))
+
+  const result: IValidateResult = { $rulesResult: {} }
+  for (const ruleKey of Object.keys(rules)) {
+    const functionParams: IFunctionParams = { value, key, parent, path, root, params }
+    const validationResult = rules[ruleKey](functionParams)
+    if (validationResult !== undefined) {
+      result.$rulesResult[ruleKey] = validationResult
+      result[ruleKey] = errors[ruleKey](functionParams)
     }
   }
+  return result
 }
--- test/unit/validate.spec.ts
+++ test/unit/validate.spec.ts
@@ -1,9 +1,9 @@
-import { validate, IFunctionParams, IInstance, ISchema } from '../../src/validate'
+import { validate, IFunctionParams, IValidateParams, IValidateResult } from '../../src/validate'
 
 const uniqueSymbol = Symbol('uniqueSymbol')
 
 test('validate', () => {
-  const params: { [key: string]: any } = {
+  const functionParams: { [key: string]: any } = {
     value: uniqueSymbol,
     key: uniqueSymbol,
     parent: uniqueSymbol,
@@ -11,53 +11,43 @@ test('validate', () => {
     path: uniqueSymbol,
     root: uniqueSymbol,
   }
-  const rule = ({
-    value: _value,
-    key: _key,
-    parent: _parent,
-    params: _params,
-    path: _path,
-    root: _root,
-  }: IFunctionParams) => {
-    params.value = _value
-    params.key = _key
-    params.parent = _parent
-    params.params = _params
-    params.path = _path
-    params.root = _root
-  }
-  const root = {}
-  const param = {}
-  const schema: ISchema = {
-    $params: {
-      param,
+  const result = {}
+  const error = {}
+  const value = {}
+  const root = value
+  const validateParams: IValidateParams = {
+    root,
+    path: [],
+    params: { param: {} },
+    rules: {
+      rule: ({ value, key, parent, params, path, root }: IFunctionParams) => {
+        functionParams.value = value
+        functionParams.key = key
+        functionParams.parent = parent
+        functionParams.params = params
+        functionParams.path = path
+        functionParams.root = root
+        return result
+      },
     },
-    $rules: {
-      required: rule,
-    },
-    $errors: {
-      required: () => {},
-    },
-  }
-  const instance: IInstance = {
-    _rootModel: root,
-    _path: '',
-    $errors: {
-      required: undefined,
-    },
+    errors: { rule: () => error },
   }
 
-  validate({ schema, instance })
-  expect(params.value).toBe(root)
-  expect(params.key).toBe(undefined)
-  expect(params.parent).toBe(undefined)
-  expect(params.path).toBe(undefined)
-  expect(params.root).toBe(root)
-  expect((params.params as any).param).toStrictEqual(param)
+  const validateResult = validate(validateParams)
+
+  expect(functionParams.value).toBe(value)
+  expect(functionParams.key).toBe(undefined)
+  expect(functionParams.parent).toBe(undefined)
+  expect(functionParams.path).toStrictEqual([])
+  expect(functionParams.root).toBe(value)
+  expect(functionParams.params).toStrictEqual(validateParams.params)
+
+  expect(validateResult.rule).toBe(error)
+  expect(validateResult.$rulesResult.rule).toBe(result)
 })
 
 test('validate (nested)', () => {
-  const params: { [key: string]: any } = {
+  const functionParams: { [key: string]: any } = {
     value: uniqueSymbol,
     key: uniqueSymbol,
     parent: uniqueSymbol,
@@ -65,71 +55,35 @@ test('validate (nested)', () => {
     path: uniqueSymbol,
     root: uniqueSymbol,
   }
-  const rule = ({
-    value: _value,
-    key: _key,
-    parent: _parent,
-    params: _params,
-    path: _path,
-    root: _root,
-  }: IFunctionParams) => {
-    params.value = _value
-    params.key = _key
-    params.parent = _parent
-    params.params = _params
-    params.path = _path
-    params.root = _root
-  }
-  const value = {}
-  const key = 'key'
-  const parent = { key: value }
-  const path = 'nested.key'
-  const root = { nested: parent }
-  const param = {}
-  const schema: ISchema = {
-    $params: {},
-    $rules: {},
-    $errors: {},
-    nested: {
-      $params: {},
-      $rules: {},
-      $errors: {},
-      key: {
-        $params: {
-          param,
-        },
-        $rules: {
-          required: rule,
-        },
-        $errors: {
-          required: () => {},
-        },
+  const result = {}
+  const error = {}
+  const validateParams: IValidateParams = {
+    root: { nested: { value: {} } },
+    path: ['nested', 'value'],
+    params: { param: {} },
+    rules: {
+      rule: ({ value, key, parent, params, path, root }: IFunctionParams) => {
+        functionParams.value = value
+        functionParams.key = key
+        functionParams.parent = parent
+        functionParams.params = params
+        functionParams.path = path
+        functionParams.root = root
+        return result
       },
     },
-  }
-  const instance: IInstance = {
-    _rootModel: root,
-    _path: '',
-    $errors: {},
-    nested: {
-      _rootModel: root,
-      _path: 'nested',
-      $errors: {},
-      key: {
-        _rootModel: root,
-        _path: 'nested.key',
-        $errors: {
-          required: undefined,
-        },
-      },
-    },
+    errors: { rule: () => error },
   }
 
-  validate({ schema: schema.nested.key, instance: instance.nested.key })
-  expect(params.value).toBe(value)
-  expect(params.key).toBe(key)
-  expect(params.parent).toBe(parent)
-  expect((params.path as string[]).join('.')).toBe(path)
-  expect(params.root).toBe(root)
-  expect((params.params as any).param).toBe(param)
+  const validateResult = validate(validateParams)
+
+  expect(functionParams.value).toBe(validateParams.root.nested.value)
+  expect(functionParams.key).toBe('value')
+  expect(functionParams.parent).toBe(validateParams.root.nested)
+  expect(functionParams.path).toStrictEqual(['nested', 'value'])
+  expect(functionParams.root).toBe(validateParams.root)
+  expect(functionParams.params).toStrictEqual(validateParams.params)
+
+  expect(validateResult.rule).toBe(error)
+  expect(validateResult.$rulesResult.rule).toBe(result)
 })
