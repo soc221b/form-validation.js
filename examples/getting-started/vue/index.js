712499bc6c81699594f6ec92556d9fc471c41f82
Ernest <iattempt.net@gmail.com>
2020-07-01 23:37:18 +0800

    docs: update example

--- examples/getting-started/vue/index.js
+++ examples/getting-started/vue/index.js
@@ -1,31 +1,45 @@
+const deepCopy = value => JSON.parse(JSON.stringify(value))
 new Vue({
   data() {
     const proxiedForm = FormValidation.proxy({ form, schema, validator })
     return {
       state: proxiedForm,
       validator,
+      djt: {},
+      isMounted: false,
     }
   },
 
   watch: {
-    'state.account1'() {
-      this.validator.account1.$v.validate()
-    },
-    'state.account2'() {
-      this.validator.account2.$v.validate()
-    },
-    'state.account3'() {
-      this.validator.account3.$v.validate()
-    },
-    'state.account4'() {
-      this.validator.account4.$v.validate()
-    },
     state: {
       immediate: true,
       deep: true,
       handler() {
+        this.validator.$v.validate()
         logInfo(this.state, this.validator)
+        this.renderDomJsonTree()
       },
+    },
+  },
+
+  mounted() {
+    this.isMounted = true
+    this.renderDomJsonTree()
+  },
+
+  methods: {
+    renderDomJsonTree() {
+      if (this.isMounted === false) return
+
+      this.$refs.self.innerHTML = ''
+      this.djt.self = new DomJsonTree(deepCopy(this.validator.$v), this.$refs.self)
+      this.djt.self.render()
+
+      for (const key in this.state) {
+        this.$refs[key].innerHTML = ''
+        this.djt[key] = new DomJsonTree(deepCopy(this.validator[key].$v), this.$refs[key])
+        this.djt[key].render()
+      }
     },
   },
 }).$mount('#app')
