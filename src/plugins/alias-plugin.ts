7ed4b0ca14fe38dd1e64d951ab33ca536094278a
Ernest <iattempt.net@gmail.com>
2020-09-15 15:49:44 +0800

    fix: alias value should be updated

--- src/plugins/alias-plugin.ts
+++ src/plugins/alias-plugin.ts
@@ -13,19 +13,42 @@ export default class AliasPlugin {
   }
 
   apply(validator: Validator) {
-    validator.$hooks.onCreated.tap('alias-plugin', () => this.alias(validator.getWrapper(validator.$path)))
+    validator.$hooks.onCreated.tap(
+      {
+        name: 'alias-plugin',
+        stage: -999,
+      },
+      () => this.alias(validator.getWrapper(validator.$path)),
+    )
   }
 
   alias(this: AliasPlugin, validationWrapper: ValidationWrapper) {
     const currentPath = validationWrapper[VALIDATOR_KEY]?.$path!
     for (const { path, aliasPath } of this.aliases) {
-      const parentWrapper = validationWrapper[VALIDATOR_KEY]?.getWrapper(currentPath.concat(aliasPath.slice(0, -1)))
-      const key = aliasPath[aliasPath.length - 1]
+      const parentWrapper = validationWrapper[VALIDATOR_KEY]?.getWrapper(currentPath.concat(path.slice(0, -1)))
+      const key = path[path.length - 1]
+
+      let lastValue = parentWrapper[key]
+      const aliasParentWrapper = validationWrapper[VALIDATOR_KEY]?.getWrapper(
+        currentPath.concat(aliasPath.slice(0, -1)),
+      )
+      const aliasKey = aliasPath[aliasPath.length - 1]
+      aliasParentWrapper[aliasKey] = lastValue
+
       Object.defineProperty(parentWrapper, key, {
         configurable: true,
         enumerable: true,
+        set(value) {
+          lastValue = value
+          const aliasParentWrapper = validationWrapper[VALIDATOR_KEY]?.getWrapper(
+            currentPath.concat(aliasPath.slice(0, -1)),
+          )
+          const aliasKey = aliasPath[aliasPath.length - 1]
+          aliasParentWrapper[aliasKey] = lastValue
+          return lastValue
+        },
         get() {
-          return validationWrapper[VALIDATOR_KEY]?.getWrapper(currentPath.concat(path))
+          return lastValue
         },
       })
     }
